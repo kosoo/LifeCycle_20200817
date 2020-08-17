af368f778dad23c90d68a36cf0c78fd663983658
이용찬 <lee9890@naver.com>
2020-08-17 19:47:26 +0900

    다른 화면 추가 생성 + 그 화면을 시작점으로 - Mainfest의 intent-filter 옮겨줌

---
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200817/BaseActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.lifecycle_20200817
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
