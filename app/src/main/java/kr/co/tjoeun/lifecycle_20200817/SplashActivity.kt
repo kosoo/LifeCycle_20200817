7262907e37900388c534ecf49c913e38a2b9c343
이용찬 <lee9890@naver.com>
2020-08-17 21:36:31 +0900

    2.5초가 지난 후에 메인화면으로 이동 - Handler 활용 예시

--- app/src/main/java/kr/co/tjoeun/lifecycle_20200817/SplashActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200817/SplashActivity.kt
@@ -1,7 +1,10 @@
 package kr.co.tjoeun.lifecycle_20200817
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : BaseActivity() {
 
@@ -16,5 +19,21 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        2.5 초(2500ms)는 로딩화면을 보여주고 나서 메인으로 이동
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+//            일정 시간이 지난 후에 실행시키고자 하는 코드 작성
+
+//        Splash 화면 => 메인화면으로 자동으로 이동해야 함
+            val myIntent = Intent(mContext, MainActivity::class.java)
+            startActivity(myIntent)
+
+//        메인화면으로 이동한 후에는 종료
+            finish()
+        }, 2500)
+
     }
 }
